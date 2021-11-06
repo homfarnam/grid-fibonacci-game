fa45d1f06fd20cb2163f6cf33c9de034ca25c860
homfarnam <homfarnam@gmail.com>
2021-11-06 22:22:56 +0330

    Add index

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import "./styles/tailwind.css"
+import "./styles/index.css"
+import "./styles/main.css"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+)

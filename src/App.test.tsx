3bc7bbb2d6a3fd7403487d7df45530aab7a89fa8
homfarnam <homfarnam@gmail.com>
2021-11-09 00:10:06 +0330

    Add new test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -15,4 +15,46 @@ describe("Click tests", () => {
       expect(cell.innerHTML).toBe("1")
     })
   })
+
+  test("click on some cells and expect to get a Fibonacci in first row", async () => {
+    render(<App />)
+    const cell = screen.getByTestId("cell-0-0")
+    const cell2 = screen.getByTestId("cell-1-2")
+    const cell3 = screen.getByTestId("cell-1-3")
+    const cell4 = screen.getByTestId("cell-1-3")
+    const cell5 = screen.getByTestId("cell-1-4")
+    const cell6 = screen.getByTestId("cell-1-4")
+    const cell7 = screen.getByTestId("cell-1-4")
+    const cell8 = screen.getByTestId("cell-1-4")
+
+    userEvent.click(cell)
+    userEvent.click(cell2)
+    userEvent.click(cell3)
+    userEvent.click(cell4)
+    userEvent.click(cell5)
+    userEvent.click(cell6)
+    userEvent.click(cell7)
+    userEvent.click(cell8)
+
+    await waitFor(() => {
+      // expect to get a Fibonacci in first row
+      expect(screen.getByTestId("cell-0-0").innerHTML).toBe("1")
+      expect(screen.getByTestId("cell-0-1").innerHTML).toBe("1")
+      expect(screen.getByTestId("cell-0-2").innerHTML).toBe("2")
+      expect(screen.getByTestId("cell-0-3").innerHTML).toBe("3")
+      expect(screen.getByTestId("cell-0-4").innerHTML).toBe("5")
+    })
+
+    // wait 2 second and expect
+
+    await waitFor(() => {
+      setTimeout(() => {
+        expect(screen.getByTestId("cell-0-0").innerHTML).toBe("0")
+        expect(screen.getByTestId("cell-0-1").innerHTML).toBe("0")
+        expect(screen.getByTestId("cell-0-2").innerHTML).toBe("0")
+        expect(screen.getByTestId("cell-0-3").innerHTML).toBe("0")
+        expect(screen.getByTestId("cell-0-4").innerHTML).toBe("0")
+      }, 2000)
+    })
+  })
 })

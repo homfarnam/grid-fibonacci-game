1faa03e5272924cc51795b10cc3b7ea21ab39c2c
homfarnam <homfarnam@gmail.com>
2021-11-08 18:56:29 +0330

    Remove unused imports

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from "react"
-import { cleanup, render, screen } from "@testing-library/react"
-import App from "./App"
+import { cleanup } from "@testing-library/react"
 import Enzyme, { shallow } from "enzyme"
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17"
 import toJson from "enzyme-to-json"
+import App from "./App"
 import { Button } from "components"
 
 Enzyme.configure({ adapter: new Adapter() })
